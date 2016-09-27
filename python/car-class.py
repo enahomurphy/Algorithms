d8fffba03ad846e54f7bd4abf6aee607dd40fc85
enaho murphy <enahomurphy@gmail.com>
2016-09-27 13:39:01 -0700

    added th bowling game algorithm

---
+++ python/car-class.py
@@ -0,0 +1,13 @@
+# You are to create a Car class that can be used to instantiate various vehicles.
+#
+# It takes in arguments that depict the type, model, and name of the vehicle,
+# provided they are set.
+#
+# Let the test guide you to building your Car boiler-plate.
+# Test Results
+
+class car(object):
+    """docstring for car."""
+    
+    def __init__(self, arg):
+        self.arg = arg
